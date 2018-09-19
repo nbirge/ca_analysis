0f10fc873759315bb65bd5400ef672991386401a
Noah <nbirge@vols.utk.edu>
2018-09-19 16:45:44 -0400

    fixex t0 code

--- wave_ops.py
+++ wave_ops.py
@@ -136,9 +136,10 @@ def fitted_trap(data,rise,top,fall,output):
 
 def find_t0(traps,output):
     length = len(traps[0])
+    rise,top.fall=100.,70.,1050.
     tr=np.arange(length)
     trap(tr,rise=100.,fall=1050.,top=70)
-    trps= np.apply_along_axis(lambda m: signal.fftconvolve(m, tr, mode='full')[0:length]/(400.*1050.), axis=1, arr=traps)
+    trps= np.apply_along_axis(lambda m: signal.fftconvolve(m, tr, mode='full')[0:length]/(rise*fall), axis=1, arr=traps)
     output[0:len(traps)]=np.argmax(trps,axis=1)
 
 
