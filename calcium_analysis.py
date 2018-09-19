fcb1faba41796ed3dbf552bf2427da9b783bcfde
Noah <nbirge@vols.utk.edu>
2018-09-19 16:35:10 -0400

    loosened convergence criterion for falltime

--- calcium_analysis.py
+++ calcium_analysis.py
@@ -119,7 +119,7 @@ if rank>0:
                 data = fr.raw(path=inpath+fname,length=length,numwaves=piece+rem,row=row+i*piece)
                 writebuffer[0:piece+rem]['result'], writebuffer[0:piece+rem]['evID'], writebuffer[0:piece+rem]['board'], writebuffer[0:piece+rem]['channel'], writebuffer[0:piece+rem]['timestamp'], writebuffer[0:piece+rem]['requesttime'] = data['result'], data['evID'], data['board'], data['channel'], data['timestamp'], data['requesttime']
                 wo.baseline_restore(data,600)        #restores baseline and performs necessary preformatting of the data (data & 16383...)
-                wo.pretrigger_rms(wave=data['wave'],workarr=maxamps,pretrig_timebin=600)
+                wo.pretrigger_rms(wave=data['wave'],workarr=maxamps[0:piece+rem],pretrig_timebin=600)
                 writebuffer[0:piece+rem]['pretrigrms']=maxamps[0:piece+rem].copy()
 #                smooth_wave= signal.filtfilt(b,a,data['wave'])
                 wo.maxes(waves=data['wave'],startpoint=500,wavelength=length,maxamps=maxamps[0:piece+rem],maxlocs=maxamps[0:piece+rem])
@@ -140,14 +140,14 @@ if rank>0:
                     writebuffer[0:piece+rem]['pilediff']=risetimes[0:piece+rem].copy()
                     writebuffer[0:piece+rem]['pileamp']=maxamps[0:piece+rem].copy()
 
-                if findt0==1:
-                    wo.find_t0(data=data,output=maxamps)
-                    writebuffer[0:piece+rem]['t0']=maxamps[0:piece+rem].copy()
-
 #                traps= np.apply_along_axis(lambda m: signal.fftconvolve(m, trap, mode='full'), axis=1, arr=data['wave'])/(rise*fall)    #NOT A GOOD WAY TO DO THIS FOR A SPECIFIC PIXEL!!!!
                 wo.apply_trap(rise=rise,data=data,trap=trap,output=traps)
                 wo.trap_energy(traps[0:piece+rem],length=length,output=maxamps[0:piece+rem])    #need these maxamps for fitting later...
                 writebuffer[0:piece+rem]['energy']=maxamps[0:piece+rem].copy()
+
+                if findt0==1:
+                    wo.find_t0(traps=data['wave'],output=maxamps)
+                    writebuffer[0:piece+rem]['t0']=maxamps[0:piece+rem].copy()
             
                 writebuffer[0:piece+rem].tofile(f)
             except ZeroDivisionError:
